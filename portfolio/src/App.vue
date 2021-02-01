0619fbf905be9eb0b879c95a0f76b75b89486140
Devin Harris <devinharris.316@gmail.com>
2021-02-01 13:26:25 -0500

    fix footer button media query and navigation cursor on sub link list

--- portfolio/src/App.vue
+++ portfolio/src/App.vue
@@ -43,11 +43,35 @@ export default {
   font-family: 'Montserrat';
 }
 
+body {
+  background-color: $theme-greydark;
+}
+
 #app {
+  overflow-x: hidden;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   height: 100%;
 }
+
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-enter {
+  &-from {
+    opacity: 0;
+    transform: scale(1.2);
+  }
+  &-to {
+    opacity: 1;
+    transform: scale(1);
+  }
+}
+
+
 </style>
